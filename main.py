0c6da60495b6d5321bff72e0eaa8147f5d44db0c
i-bot-faba <bitrixbuh25@gmail.com>
2025-03-23 15:08:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
@@ -12,15 +13,18 @@ async def delete_trading_message(update: Update, context: ContextTypes.DEFAULT_T
                 message_id=update.message.message_id
             )
 
-async def on_startup(app):
-    # Замените <your-app-name> на имя вашего приложения на Render
-    webhook_url = f"https://<your-app-name>.onrender.com/webhook"
-    await app.bot.set_webhook(webhook_url)
-
-if __name__ == '__main__':
+async def main():
     port = int(os.environ.get("PORT", 8443))
     app = ApplicationBuilder().token("7712516662:AAECNpqvIMy1OszmAAQxrBEuHYhQc52v4kQ").build()
     
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, delete_trading_message))
     
-    app.run_webhook(listen="0.0.0.0", port=port, url_path="webhook", on_startup=on_startup)
+    # Укажи правильный URL: замените <your-app-name> на имя приложения на Render
+    webhook_url = f"https://<your-app-name>.onrender.com/webhook"
+    await app.bot.set_webhook(webhook_url)
+    
+    await app.run_webhook(listen="0.0.0.0", port=port, url_path="webhook")
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
